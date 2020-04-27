d952518ac8386e7094ee636b1982de35838a898c
MohamedEsssam <me547010@gmail.com>
2020-04-27 14:55:31 +0200

    Add manhattanDistance & euclideanDistance

--- src/Heuristics.java
+++ src/Heuristics.java
@@ -1,3 +1,29 @@
 public class Heuristics {
+    public static int manhattanDistance(State currentState) {
+        int cost = 0, index = 0;
+        int xValue, yValue;
+        for (int i=0; i < currentState.getGameState().size(); i++)
+            for (int j = 0; j < currentState.getGameState().size(); j++) {
+                xValue = currentState.getGameState().get(index) / currentState.getGameState().size();
+                yValue = currentState.getGameState().get(index) % currentState.getGameState().size();
+                cost += Math.abs(i - xValue) + Math.abs(j - yValue);
+                index++;
+            }
 
+        return cost;
+    }
+
+    public static int euclideanDistance(State currentState) {
+        int cost = 0, index = 0;
+        int xValue, yValue;
+        for (int i=0; i < currentState.getGameState().size(); i++)
+            for (int j = 0; j < currentState.getGameState().size(); j++) {
+                xValue = currentState.getGameState().get(index) / currentState.getGameState().size();
+                yValue = currentState.getGameState().get(index) % currentState.getGameState().size();
+                cost += Math.sqrt(Math.pow(i - xValue, 2) + Math.pow(j - yValue, 2));
+                index++;
+            }
+
+        return cost;
+    }
 }
